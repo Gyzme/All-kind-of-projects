5667a74cf129b00b8ed0eb91fcfd2b06a7f38c56
Gyzme <x.snoeck@gmail.com>
2026-06-22 02:09:48 +0200

    New  village project

---
+++ Village/src/main/java/Programme.java
@@ -0,0 +1,7 @@
+public class Programme {
+    public static void main (String[]args){
+        Personnes jean = new Personnes("Jean", "DuMoulin", "boulanger", 32);
+        Batiments boulangerie = new Batiments ("boulangerie");
+
+    }
+}
